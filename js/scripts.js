f4ea6d683b0fcfa5df8b294a97b6539d18feefd9
SavelySS <sssligr@gmail.com>
2024-11-04 22:05:14 +0700

    исправления в Задача 1. Формирование каркаса сайта

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,9 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log('Working...');
 });
+const button = document.querySelector('.left-menu-top__main__btn');
+const icon = document.querySelector('.dir-up');
+
+button.addEventListener('click', () => { 
+    icon.classList.toggle('rotate');
+});
