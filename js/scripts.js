89a79b63f0c85376985ecb5a44e56290418dc455
SavelySS <sssligr@gmail.com>
2024-11-06 00:33:30 +0700

    исправления 2 в Задача 1. Формирование каркаса сайта

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,21 @@
 
 document.addEventListener('DOMContentLoaded', () => {
     console.log('Working...');
-});
-const button = document.querySelector('.left-menu-top__main__btn');
-const icon = document.querySelector('.dir-up');
 
-button.addEventListener('click', () => { 
-    icon.classList.toggle('rotate');
+    const katalog_img = document.querySelector('.header-bot__fish');
+    var theDropdown = document.getElementById('btn-group');
+    theDropdown.addEventListener('show.bs.dropdown', function () {
+        katalog_img.src = 'assets/icons/katalog-remove.svg';
+        document.getElementsByClassName('header-bot__fish')[0].style.margin = "15px 8px 15px 32px";
+    });
+    theDropdown.addEventListener('hide.bs.dropdown', function () {
+        katalog_img.src = 'assets/icons/katakog-fish.svg';
+        document.getElementsByClassName('header-bot__fish')[0].style.margin = "11px 8px 16px 32px";
+    });
+
+    const left_menu_button = document.querySelector('.left-menu-top__main__btn');
+    const icon = document.querySelector('.dir-up');
+    left_menu_button.addEventListener('click', () => { 
+        icon.classList.toggle('rotate');
+    });
 });
