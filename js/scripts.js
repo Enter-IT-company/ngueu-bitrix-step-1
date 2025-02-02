32dc46619dbb990253f02036401cdd997df1a37d
Arbalest278 <123440603+Arbalest278@users.noreply.github.com>
2025-02-02 19:25:04 +0300

    first commit

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,37 @@
 
 document.addEventListener('DOMContentLoaded', () => {
     console.log('Working...');
+
+    // Инициализация слайдеров
+    const heroSwiper = new Swiper('.hero-slider .swiper', {
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+    });
+
+    const newArrivalsSwiper = new Swiper('.new-arrivals-swiper', {
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+    });
+
+    const popularProductsSwiper = new Swiper('.popular-products-swiper', {
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+    });
+
+    const recipesSwiper = new Swiper('.recipes-swiper', {
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+    });
 });
